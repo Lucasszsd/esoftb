ea8ccc3243108bc46930971ff1ecdc49aa9b05fa
Lucas Dutra <94666925+Lucasszsd@users.noreply.github.com>
2024-03-24 18:35:35 -0300

    Update book.controller.ts
    Quantidade de instâncias de bookcontroller diminuida

--- 13-03-2024/src/books/book.controller.ts
+++ 13-03-2024/src/books/book.controller.ts
@@ -2,25 +2,31 @@ import { Request, Response } from 'express'
 import { BookService } from './book.service'
 
 class BookController {
+    private bookService: BookService;
+
+    constructor() {
+        this.bookService = new BookService();
+    }
+
     async create(req: Request, res: Response) {
-        const book = await new BookService().create(req.body)
-        return res.json(book)
+        const book = await this.bookService.create(req.body);
+        return res.json(book);
     }
 
     async get(req: Request, res: Response){
-        const getBook =  await new BookService().get(req.params)
-        return getBook;
-
+        const getBook =  await this.bookService.get(req.params);
+        return res.json(getBook);
     }
 
     async putBook(req: Request, res: Response){
-        const putBook =  await new BookService().put(req.body)
+        const putBook =  await this.bookService.put(req.body);
         return res.json(putBook);
     }
+
     async deleteBook(req: Request, res: Response){
-        const deleteBook = await new BookService().delete(req.params)
-        return res.json(deleteBook)
+        const deleteBook = await this.bookService.delete(req.params);
+        return res.json(deleteBook);
     }
 }
 
-export default new BookController()
+export default new BookController();
